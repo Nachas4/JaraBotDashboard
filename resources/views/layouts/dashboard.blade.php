d9084cf4c4e5b08ebc2a12c779c6635a6a72c4a6
Klozon-cs <csiszarmatyas3@gmail.com>
2024-03-03 17:34:19 +0100

    Cleaned,Fully responsive, Dashboard with Toggle menu

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -27,19 +27,31 @@
 </head>
 
 <body class="bg--black overflow-hidden">
-    <div class="d-flex" style="height:100vh; max-width:100vw">
+    <div class="d-flex vh-100">
 
-        {{-- MENU --}}
-        <div class="menu h-100 p-3">
-            <div class="card-- p-4 h-100 " style="overflow: scroll;">
+        {{-- Sidebar Lable --}}
+        <div class="d-flex justify-content-end bg--black rounded menu-mini d-lg-none" id="menu-mini" onclick="toggle()">
+            <i class="fa-solid fa-bars fs-4 p-2"></i>
+        </div>
+
+        {{-- MENU  on Left --}}
+        <div class="menu-closed h-100 p-3" style="z-index:10;" id="menu">
+            <div class="card-- p-4 h-100 overflow-auto">
                 <div class="d-flex flex-column justify-content-between h-100">
-                    <a class="text-white nav-logo" href="{{ url('/') }}" style="font-size: 30px;"
-                        style="height:10%;">
+
+                    {{-- Menu Close Lable --}}
+                    <i class="fa-solid fa-minus fs-5 p-2 d-lg-none position-absolute"
+                        style="right:10px; top:20px;cursor: pointer;" onclick="toggle()"></i>
+
+                    {{-- Brand --}}
+                    <a class="text-white nav-logo" href="{{ url('/') }}" style="font-size: 30px;">
                         <img src="{{ asset('log6.png') }}" class="img-fluid me-1"
                             style="height:60px;margin-left:-10px;">
                         JaraBots
                     </a>
 
+
+                    {{-- User Datas --}}
                     <div class="card--holder p-3 d-flex align-items-center w-100 rounded mt-4 mb-2"
                         style="height:80px;">
                         <img src="{{ asset('pfp-2.jpg') }}" class="img-fluid me-2 rounded-circle h-100">
@@ -48,28 +60,42 @@
                                 style="height: min-content; margin-bottom:-10px;">Klozon</span>
                             <span class="fs-6" style="height: min-content;font-size:80%;">csiszár</span>
                         </div>
-                        <i class="text--neon ms-auto me-1 fa-solid fa-arrow-right-from-bracket"></i>
 
+                        {{-- Log Out --}}
+                        <a href="" class="ms-auto ">
+                            <i class="text--neon me-1 fa-solid fa-arrow-right-from-bracket"></i>
+                        </a>
                     </div>
 
 
+                    {{-- Navigation --}}
                     <div class="d-flex flex-column justify-content-around text--grey w-100 rounded mt-2 mb-2"
                         style="height:160px;">
                         <div class="fs-5 ms-4">
-                            <i class="fa-solid fa-house me-2" style="color:rgb(136, 224, 227);"></i><span
-                                style="color:rgb(136, 224, 227);">Dashboard</span>
+                            <a href="">
+                                <i class="fa-solid fa-house me-2" style="color:rgb(136, 224, 227);"></i><span
+                                    style="color:rgb(136, 224, 227);">Dashboard</span>
+                            </a>
                         </div>
                         <div class="fs-5 ms-4">
-                            <i class="fa-solid fa-robot me-2"></i><span>Bots</span>
+                            <a href="">
+                                <i class="fa-solid fa-robot me-2"></i><span>Bots</span>
+                            </a>
                         </div>
                         <div class="fs-5 ms-4">
-                            <i class="fa-regular fa-user me-2"></i><span>Your Profile</span>
+                            <a href="">
+                                <i class="fa-regular fa-user me-2"></i><span>Your Profile</span>
+                            </a>
                         </div>
                         <div class="fs-5 ms-4">
-                            <i class="fa-solid fa-gear me-2"></i><span>Settings</span>
+                            <a href="">
+                                <i class="fa-solid fa-gear me-2"></i><span>Settings</span>
+                            </a>
                         </div>
                     </div>
                     <hr>
+
+                    {{-- Notifications --}}
                     <div class="d-flex mt-2 mb-3 w-100 justify-content-between ">
                         <span>Notifications</span>
                         <a class="text-secondary">View All ></a>
@@ -97,14 +123,27 @@
                     </div>
 
 
+
+                    {{-- Log Out --}}
                     <div class="text--neon w-100 text-center mt-auto">
-                        <i class="fa-solid fa-arrow-right-from-bracket"></i><a class="btn btn-link">Log Out</a>
+                        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                        <a href="" class="btn btn-link">LogOut</a>
                     </div>
 
                 </div>
             </div>
         </div>
 
+
+
+
+
+
+
+
+
+
+        {{-- Right  Side --}}
         <div class="d-flex flex-column p-3 flex-fill">
 
             {{-- <div class="card-- d-flex justify-content-start p-3 w-100" style="width: 100%;height: 150px; gap:15px;overflow:scroll; ">
@@ -126,14 +165,11 @@
                 <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded">
                 <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded">
                 <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded">
-
-
-    
             </div> --}}
 
             {{-- SERVERS SLIDER --}}
-            <div class="card-- p-3 mb-4" style="width: 100%;height: 150px;">
-                <div class="d-flex justify-content-start h-100 w-100" style="overflow-y: hidden;  overflow-x: auto; "
+            <div class="card-- p-3 mb-4 w-100" style="height: 150px;z-index:2;">
+                <div class="d-flex justify-content-start h-100 w-100 overflow-auto" style="overflow-y: hidden;"
                     id="servers">
 
                     <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
@@ -164,65 +200,55 @@
                         draggable="false">
                     <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
                         draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
-                    <img src="{{ asset('pfp-1.png') }}" alt="" class="server--list img-fluid rounded"
-                        draggable="false">
+
 
                 </div>
-
-
             </div>
             <main class="w-100 h-100">
                 <div class="card-- d-flex flex-column w-100 h-100">
-                    <div class="card--header text-white p-2 mb-3">
-                        <div class="row">
-                            <div class="col-3 text-decoration-underline"><b>Some shit</b></div>
-                            <div class="col-3">fuck this</div>
-                            <div class="col-3">CSS -_-</div>
-                            <div class="col-3">sdf</div>
-                        </div>
-                    </div>
-                    <div class="card--body p-3 h-100 text-white rounded">
-                        {{-- CONTENT --}}
-                        @yield('content')
-                    </div>
-                </div>
+                    {{-- CONTENT --}}
+                    @yield('content')
 
+
+                    {{-- Card Dasboard Sample --}}
+                    {{-- <div class="card-- d-flex flex-column w-100 h-100">
+                        <div class="card--header text-white p-2 mb-3">
+                            <div class="row">
+                                <div class="col-3 text-decoration-underline"><b>Some shit</b></div>
+                                <div class="col-3">fuck this</div>
+                                <div class="col-3">CSS -_-</div>
+                                <div class="col-3">sdf</div>
+                            </div>
+                        </div>
+                        <div class="card--body p-3 h-100 text-white rounded">
+                            
+                        </div>
+                    </div> --}}
+
+                </div>
             </main>
         </div>
-
     </div>
 
 </body>
 
 </html>
+
+<style>
+    @media screen and (max-width: 992px) {
+        .menu-closed {
+            transform: translateX(-100%);
+            position: absolute;
+        }
+    }
+
+    @media screen and (max-width: 576px) {
+        .menu-open {
+            min-width: 100vw;
+        }
+    }
+</style>
+
 <script>
     //horizontal scrolling with mouse wheel
     let servers = document.getElementById('servers');
@@ -236,4 +262,13 @@
     }, {
         passive: true
     });
+
+    //Toggle Menu
+    let menu = document.getElementById('menu');
+    let menumini = document.getElementById('menu-mini');
+
+    let toggle = () => {
+        menu.classList.toggle('menu-open');
+        menumini.classList.toggle('menu-mini-closed');
+    }
 </script>
--- resources/views/servers.blade.php
+++ resources/views/servers.blade.php
@@ -1,7 +1,90 @@
 @extends('layouts.dashboard')
 
 @section('content')
+    <div class="card--header text-white p-2 mb-3">
+        <div class="row">
+            <div class="col-12 text-center fs-3"><b>XYZ server settings</b></div>
+        </div>
+    </div>
+    <div class="card--body p-3 h-100 text-white rounded overflow-auto">
+
+        <div class="row">
+            <hr>
+            <div class="col-12 col-xl-4 col-md-6 mb-4">
+                <h3>Server status:</h3>
+                <input class="form-control" type="text" name="asd" id="">
+                <label for="">Example here!</label>
+            </div>
+            <div class="col-12 col-xl-4 col-md-6 mb-4">
+                <h3>Server status:</h3>
+                <input class="form-control" type="text" name="asd" id="">
+                <label for="">Example here!</label>
+            </div>
+
+            <div class="col-12 col-xl-4 col-md-6 mb-4">
+                <h3>Server status:</h3>
+                <select class="form-control">
+                    <option>Default select</option>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                    <option>Option 4</option>
+                </select>
+                <label for="">Example here!</label>
+            </div>
+            <hr>
 
 
 
+            <div class="col-12 col-xl-4 col-md-6 mb-4">
+                <h3>Server status:</h3>
+                <input class="form-control" type="text" name="asd" id="">
+                <label for="">Example here!</label>
+            </div>
+            <div class="col-12 col-xl-4 col-md-6 mb-4">
+                <h3>Server status:</h3>
+                <input class="form-control" type="text" name="asd" id="">
+                <label for="">Example here!</label>
+            </div>
+
+            <div class="col-12 col-xl-4 col-md-6 mb-4">
+                <h3>Server status:</h3>
+                <select class="form-control">
+                    <option>Default select</option>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                    <option>Option 4</option>
+                </select>
+                <label for="">Example here!</label>
+            </div>
+            <hr>
+
+            <div class="col-12 col-xl-4 col-md-6 mb-4">
+                <h3>Server status:</h3>
+                <input class="form-control" type="text" name="asd" id="">
+                <label for="">Example here!</label>
+            </div>
+            <div class="col-12 col-xl-4 col-md-6 mb-4">
+                <h3>Server status:</h3>
+                <input class="form-control" type="text" name="asd" id="">
+                <label for="">Example here!</label>
+            </div>
+
+            <div class="col-12 col-xl-4 col-md-6 mb-4">
+                <h3>Server status:</h3>
+                <select class="form-control">
+                    <option>Default select</option>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                    <option>Option 4</option>
+                </select>
+                <label for="">Example here!</label>
+            </div>
+            <hr>    
+        </div>
+
+
+    </div>
 @endsection
