870d2948edcf78b53745617e4dc0aee8db140bf5
Nachas4 <nachas4@tuta.io>
2024-04-17 11:31:14 +0200

    Updated api.php
    Removed endpoints because they're not needed anymore

--- routes/api.php
+++ routes/api.php
@@ -18,27 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('getAll/{table}/{field}/{value}', function ($table, $field, $value) {
-    try {
-        $all = DB::table($table)
-            ->where($field, $value)
-            ->get();
-        
-        return response()->json($all);
-    } catch (\Throwable $th) {
-        return response()->json($th->getMessage(), 500);
-    }
-});
-
-Route::get('getOne/{table}/{field}/{value}/{getField}', function ($table, $field, $value, $getField) {
-    try {
-        $one = DB::table($table)
-            ->where($field, $value)
-            ->get($getField);
-
-        return response()->json($one);
-    } catch (\Throwable $th) {
-        return response()->json($th->getMessage(), 500);
-    }
-});
