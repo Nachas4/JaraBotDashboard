dc47ad10053c21becdce46caf55cc927d3ec84a8
Nachas4 <nachas4@tuta.io>
2024-05-12 11:49:35 +0200

    Updated api.php for the last time

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Models\AutoRole;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,20 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::get('getAll/{table}/{field}/{value}', function ($table, $field, $value) {
-    $all = DB::table($table)
-        ->where($field, $value)
-        ->get();
-
-    return response()->json($all);
-});
-
-Route::get('getOne/{table}/{field}/{value}/{getField}', function ($table, $field, $value, $getField) {
-    $one = DB::table($table)
-        ->where($field, $value)
-        ->get($getField);
-        
-    return response()->json($one);
 });
